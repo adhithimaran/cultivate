e52e0b214e7fae771eff64ecb301e2f18c9fe20a
Adhithi Maran <adhithimaran@gmail.com>
2026-03-10 22:50:28 -0500

    LoginScreen file created

---
+++ app/src/main/java/com/adhithimaran/cultivate/auth/LoginScreen.kt
@@ -0,0 +1,22 @@
+package com.adhithimaran.cultivate.auth
+
+import android.app.Activity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+
+class LoginScreen {
+}
